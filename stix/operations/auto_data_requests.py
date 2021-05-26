33b1b3c0d332bd155fb2b7fcc29b4a469082e5af
xiaohl <dr.hualinxiao@gmail.com>
2021-05-26 21:26:51 +0200

    auto data request added

--- stix/operations/auto_data_requests.py
+++ stix/operations/auto_data_requests.py
@@ -15,8 +15,9 @@ EMAX_MAP=[8, 13, 17, 23, 28] # emax if there are prominence the corresponding li
 logger = stix_logger.get_logger()
 db=mdb.get_db()
 bsd_form=db['bsd_req_forms']
-flare_collection=db['flare_tbc']
-TIME_MARGIN=120
+flare_collection=db['flares_tbc']
+L4_TIME_MARGIN=120
+L1_TIME_MARGIN=300
 
 L4_REQUEST_GROUP_MAX_TIME_GAP=3600
 
@@ -25,18 +26,20 @@ def create_requests(flare_docs):
     if len(flares)==0:
         print('No flares selected')
         return
-    for doc in flare_docs:
+    for doc in flares:
         create_l1_request(doc)
+    create_l4_groups(flares)
+
 def create_l4_groups(flare_docs):
     group=[]
     groups=[]
-    last_time=flares[0]['peak_unix_time']
+    last_time=flare_docs[0]['peak_unix_time']
     for doc in flare_docs:
-        create_l1_request(doc)
-        this_time=flares[0]['peak_unix_time']
+        this_time=doc['peak_unix_time']
         if this_time-last_time>L4_REQUEST_GROUP_MAX_TIME_GAP:
             groups.append(group)
             group=[]
+        last_time=this_time
         group.append(doc)
     print('number of L4 groups', len(groups))
     
@@ -57,7 +60,7 @@ def create_l4_groups(flare_docs):
         except:
             pass
         create_template(flare_ids, flare_entry_ids, run_ids, start_utc, 
-                duration, emax, left_margin=TIME_MARGIN, right_margin=TIME_MARGIN, level=4)
+                duration, emax, left_margin=-L4_TIME_MARGIN, right_margin=L4_TIME_MARGIN, level=4)
 
         
 
@@ -65,11 +68,9 @@ def create_l4_groups(flare_docs):
 
 
 def create_l1_request(doc):
-
-
     flare_id=doc['flare_id']
     flare_entry_id=doc['_id']
-    start_utc=doc['start_utc']
+    start_utc=stix_datetime.unix2utc(doc['start_unix'])
     duration=int(doc['duration'])
     flare_entry_ids=doc['_id']
     run_ids=doc['run_id']
@@ -79,15 +80,12 @@ def create_l1_request(doc):
         emax=EMAX_MAP[ilc]
     except:
         pass
-
-    
-
-    data=create_template(flare_id, flare_entry_ids, run_ids, start_utc, duration, emax, TIME_MARGIN, TIME_MARGIN, level=1)
-    print(f'Created a request for Flare {flare_id}, ID:{current_id}')
+    create_template(flare_id, flare_entry_ids, run_ids, start_utc, duration, emax, -L1_TIME_MARGIN, L1_TIME_MARGIN, level=1)
 
 def create_template(flare_ids, flare_entry_ids, run_ids, start_utc, duration, emax=13, left_margin=0, right_margin=0,
         level=1):
-    if list(bsd_form.find().sort(
+    level_name=DATA_LEVEL_NAMES[level]
+    if list(bsd_form.find({'flare_id':flare_ids, 'request_type':level_name}).sort(
         '_id', -1)):
         print(f'data request for Flare {flare_ids} already exists.')
         return
@@ -104,9 +102,15 @@ def create_template(flare_ids, flare_entry_ids, run_ids, start_utc, duration, em
     if left_margin!=0:
         start_utc=stix_datetime.unix2utc(stix_datetime.utc2unix(start_utc)+left_margin)
 
+    if isinstance(flare_ids, list):
+        if len(flare_ids)==1:
+            flare_ids=flare_ids[0]
+
     duration=int(duration-left_margin+right_margin)
-    detector_mask=='0xFFFFFFFF' if level==1 else '0xFFFFFCFF'
+    detector_mask='0xFFFFFFFF' if level==1 else '0xFFFFFCFF'
     num_detectors = 32 if level==1 else 30
+    emin=0
+    eunit=1
     num_pixels = 12
     tunit=1
     tmax=duration
@@ -119,9 +123,8 @@ def create_template(flare_ids, flare_entry_ids, run_ids, start_utc, duration, em
         data_volume = 1.1 * T * (M * P * (E + 4) + 16)
     elif level == 4:
         data_volume = 1.1 * T * (E + 4)
-    level_name=DATA_LEVEL_NAMES[level]
 
-    return {
+    form={
     "data_volume" : str(math.floor(data_volume)),
     "execution_date" : '',
     "author" : "Hualin",
@@ -135,35 +138,43 @@ def create_template(flare_ids, flare_entry_ids, run_ids, start_utc, duration, em
     "flare_id" : flare_ids,
     'flare_entry_ids':flare_entry_ids,
     "detector_mask" : detector_mask, 
+    "creation_time": datetime.utcnow(),
     "pixel_mask" : "0xFFF",
-    "emin" : "0",
+    "emin" : str(emin),
     "emax" : str(emax),
     'hidden':False,
     'run_id': run_ids,
     'status':0,
-    "eunit" : "1",
+    "eunit" : str(eunit),
     '_id':current_id, 
     "description" : f"{level_name} request for Flare {flare_ids}",
     "volume" : str(int(data_volume)),
     "unique_ids" : []
         }
-    bsd_form.insert_one(data)
-def create_data_request_for_files(file_ids, cnts_threshold=400):
-    flares=flare_collection.find({'run_id':{'$in': file_ids}, {'peak_counts':{'$gt':cnts_threshold}}, 'hidden':False}).sort('peak_unix_time',1)
+    print(f'Inserting request {form["_id"]}, type: {level_name}')
+    bsd_form.insert_one(form)
+#def create_data_request_for_files(file_ids, cnts_threshold=400):
+#    flares=flare_collection.find({'run_id':{'$in': file_ids}, 'peak_counts':{'$gt':cnts_threshold}, 'hidden':False}).sort('peak_unix_time',1)
+#    create_requests(flares)
+
+def create_data_request_for_flares(start_flare_id, end_flare_id, cnts_threshold=400):
+    query={'_id':{'$gte': start_flare_id, '$lte':end_flare_id},  'peak_counts':{'$gt':cnts_threshold}, 'hidden':False}
+    flares=flare_collection.find(query).sort('peak_unix_time',1)
+    print('counts',flares.count())
     create_requests(flares)
 
-def create_data_request_for_flares(flare_ids, cnts_threshold=400):
-    flares=flare_collection.find({'_id':{'$in': flare_ids}, {'peak_counts':{'$gt':cnts_threshold}}, 'hidden':False}).sort('peak_unix_time',1)
-    create_requests(flares)
 
 
-
-def __name__=='__main__':
+if __name__=='__main__':
     if len(sys.argv)<3:
-        print('auto_req <begin_flare_id> <end_flare_id>')
+        print('auto_req <begin_flare_id> <end_flare_id> [threshold:400]')
     else:
-        flare_ids=[x in range(int(sys.argv[1]), int(sys.argv[2]))]
-        create_data_request_for_flares(flare_ids)
+        threshold=400
+        if len(sys.argv)==4:
+            threshold=int(sys.argv[3])
+        start_id=int(sys.argv[1])
+        end_id=int(sys.argv[2])
+        create_data_request_for_flares(start_id, end_id)
 
 
     
