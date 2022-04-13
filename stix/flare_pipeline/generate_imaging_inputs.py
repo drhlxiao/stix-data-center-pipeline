5139441174f8c6e9965ef0ca70cea8f1e6fad249
dr.hualinxiao@gmail.com <dr.hualinxiao@gmail.com>
2022-04-13 11:46:26 +0200

    updated imaging wrapper

--- stix/flare_pipeline/generate_imaging_inputs.py
+++ stix/flare_pipeline/generate_imaging_inputs.py
@@ -3,9 +3,9 @@
 """
 import os
 import sys
-import hissw
 import json
 import numpy as np
+import subprocess
 from datetime import datetime, timedelta
 
 from astropy import units as u
@@ -46,6 +46,30 @@ IDL_SCRIPT_PATH=Path(__file__).resolve().parent.parent/ 'idl'
 from pprint import pprint
 HOST='https://datacenter.stix.i4ds.net/'
 
+def execute_script(shell_filename, verbose=False):
+    """
+    execute script
+    """
+    subprocess.call(['chmod', 'u+x', shell_filename])
+    cmd_output = subprocess.run([shell_filename], shell=True, stderr=subprocess.PIPE,
+                                stdout=subprocess.PIPE)
+    check_for_errors(cmd_output, verbose)
+
+
+def check_for_errors(output, verbose):
+    """
+    Check IDL output to try and decide if an error has occurred
+    """
+    stdout = output.stdout.decode('utf-8')
+    stderr = output.stderr.decode('utf-8')
+    # NOTE: For some reason, not only errors are output to stderr so we
+    # have to check it for certain keywords to see if an error occurred
+    if 'execution halted' in stderr.lower():
+        raise RuntimeError(stderr)
+    if 'failed to acquire license' in stderr.lower():
+        raise RuntimeError(stderr)
+    if verbose:
+        print(f'{stderr}\n{stdout}')
 
 def generate_inputs_for_science_data(bsd_ids=[]):
     if not bsd_ids:
@@ -65,7 +89,7 @@ def call_idl(inputs, bkg_fits, sig_fits):
             f';{HOST}/download/fits/filename/{bkg_filename}',
             f';{HOST}/download/fits/filename/{sig_filename}',
                 f'.run {IDL_SCRIPT_PATH}/stix_image_reconstruction.pro',
-                f'stix_image_construct, {parameters}',
+                f'stx_image_reconstruct, {parameters}',
                 'exit']
     sc_fname=os.path.join(IDL_SCRIPT_PATH, 'top.pro')
     f=open(sc_fname, 'w')
@@ -73,6 +97,15 @@ def call_idl(inputs, bkg_fits, sig_fits):
         f.write(l+'\n')
     print('updated', sc_fname)
     f.close()
+    print("executing script")
+    try:
+        execute_script(IDL_SCRIPT_PATH/'stix_imaging.sh')
+    except RuntimeError:
+        print("Run time error!")
+        return False
+    
+    print("success!")
+    return True
 
 
         
@@ -159,29 +192,19 @@ def generate_imaging_inputs(doc, min_counts=5000,
         tb['fits']=[[]]*len(imaging_energies)
         for ie, energy in enumerate(imaging_energies):
             fits_prefix=f'sci_{bsd_id}_uid_{uid}_{ibox}_{ie}'
-            out_fits_fnames=[fits_prefix+ext for ext in ['_fwfit.fits', '_bp.fits']]
+            out_fits_fnames=[os.path.join(quicklook_path, fits_prefix+ext) for ext in ['_fwfit.fits', '_bp.fits']]
             num_images+=1
             if tb['counts_enough'][ie]:
-                call_idl([bkg_fname, fname, tb['utc_range'][0],tb['utc_range'][1],
+                success=call_idl([bkg_fname, fname, tb['utc_range'][0],tb['utc_range'][1],
                     tb['energy_range_sci'][ie][0],
                     tb['energy_range_sci'][ie][1],
                     out_fits_fnames[0],
                     out_fits_fnames[1],
-                    L0, B0, rsun,roll], bkg_fname, fname)
-                """
-                inputs = {'background_L1A_fits_filename': bkg_fname,
-                  'flaring_data_L1A_fits_filename': fname,
-                  'flare_start_UTC': tb['utc_range'][0],
-                  'flare_end_UTC': tb['utc_range'][1],
-                  'energy_range_science_channel_upper_limit': tb['energy_range_sci'][0],
-                  'energy_range_science_channel_lower_limit':  tb['energy_range_sci'][1],
-                  'vis_fwdfit_map_filename': out_fits_fnames[0],
-                  'bp_map_filename': out_fits_fnames[1], 
-                  'L0': L0,
-                  'B0': B0,
-                  'apparent_radius_sun': rsun,
-                  'roll_angle_solo': roll}
-                  """
+                    round(L0,4), round(B0,4), round(rsun,4),round(roll,4)], bkg_fname, fname)
+                if not success:
+                    continue
+                print("success, output:", out_fits_fnames)
+                
                 tb['fits'][ie]=out_fits_fnames
 
     if num_images==0:
