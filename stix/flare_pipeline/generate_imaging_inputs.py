b4cc5ec8c305dadbbd9374707614c04ce22bc910
dr.hualinxiao@gmail.com <dr.hualinxiao@gmail.com>
2022-04-13 11:10:28 +0200

    bug fixes

--- stix/flare_pipeline/generate_imaging_inputs.py
+++ stix/flare_pipeline/generate_imaging_inputs.py
@@ -44,6 +44,7 @@ IDL_SCRIPT_PATH=Path(__file__).resolve().parent.parent/ 'idl'
 
 
 from pprint import pprint
+HOST='https://datacenter.stix.i4ds.net/'
 
 
 def generate_inputs_for_science_data(bsd_ids=[]):
@@ -55,9 +56,14 @@ def generate_inputs_for_science_data(bsd_ids=[]):
     for doc in docs:
         generate_imaging_inputs(doc)
         
-def call_idl(inputs):
-    parameters=','.join([f'"{x}"'if isinstance(x, str) else str(x) for x in inputs])
+def call_idl(inputs, bkg_fits, sig_fits):
+    parameters=','.join([f"'{x}'" if isinstance(x, str) else str(x) for x in inputs])
+    bkg_filename=os.path.basename(bkg_fits)
+    sig_filename=os.path.basename(sig_fits)
     script_lines=["!PATH=!PATH",
+            f'; The two fits files can be downloaded via the links below:',
+            f';{HOST}/download/fits/filename/{bkg_filename}',
+            f';{HOST}/download/fits/filename/{sig_filename}',
                 f'.run {IDL_SCRIPT_PATH}/stix_image_reconstruction.pro',
                 f'stix_image_construct, {parameters}',
                 'exit']
@@ -161,7 +167,7 @@ def generate_imaging_inputs(doc, min_counts=5000,
                     tb['energy_range_sci'][ie][1],
                     out_fits_fnames[0],
                     out_fits_fnames[1],
-                    L0, B0, rsun,roll])
+                    L0, B0, rsun,roll], bkg_fname, fname)
                 """
                 inputs = {'background_L1A_fits_filename': bkg_fname,
                   'flaring_data_L1A_fits_filename': fname,
