329c94140172aea72f3945802c52cc76b32bae5e
dr.hualinxiao@gmail.com <dr.hualinxiao@gmail.com>
2022-05-20 13:06:59 +0200

    updated task manager

--- stix/pipeline/task_manager.py
+++ stix/pipeline/task_manager.py
@@ -3,16 +3,23 @@
 
 import multiprocessing
 import time
-from stix.core import logger
 
+from stix.core import logger
+from threading import Timer
 logger = logger.get_logger()
+def kill_me(p, name):
+    logger.info(f"Timeout! Terminating {name}  !")
+    try:
+        if p.is_alive():
+            logger.info(f"Killing it!")
+            p.terminate()
+    except Exception:
+        pass
 
 def run_on_background(func, name="process", args=(), wait=3600*3):
     logger.info(f"Running {name} on background!")
     p = multiprocessing.Process(target=func, name=name, args=args)
     p.start()
-    p.join(wait)
-    if p.is_alive():
-        #kill it after time out
-        logger.info(f"Timeout Terminating {name} !")
-        p.terminate()
+    t=Timer(wait, kill_me, [p, name])
+    t.start()
+
