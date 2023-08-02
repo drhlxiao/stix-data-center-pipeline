5c5ba1df54d8f539880a63ad7c63f1cb539f3d67
dr.hualinxiao@gmail.com <dr.hualinxiao@gmail.com>
2023-08-02 17:40:55 +0200

    updated energy bins

--- stix/core/energy_bins.py
+++ stix/core/energy_bins.py
@@ -1,5 +1,7 @@
-#a class to store STIX energy bins
-# it has been updated 
+#A python class to manage STIX energy bins
+#it needs to be maintained 
+# History:
+#    Created on 2023-08-02 by  Hualin Xiao 
 import time
 
 import numpy as np
@@ -14,13 +16,13 @@ class StixEnergyBins:
 
     _history = [
         {
-            'time_ranges': [1690761600, MISSION_END_TIME],  # 2023-07-31T00:00:00 to sometime in the future
+            'time_range': [1690761600, MISSION_END_TIME],  # 2023-07-31T00:00:00 to sometime in the future
             'energy_bins': [0.0, 4.0, 5.5, 6.3, 6.56, 6.8, 7.3, 8.0, 9.0,
                             10.0, 11.0, 13.0, 15.0, 17.0, 19.0, 21.0, 23.0, 25.0,
                             28.0, 32.0, 36.0, 40.0, 45.0, 50.0, 56.0, 63.0, 70.0, 76.0, 84.0, 100.0, 120.0, 150.0, np.inf]
         },
         {
-            'time_ranges': [1675036800, 1675353600],  # 2023-01-30 to 2023-02-02T16:00
+            'time_range': [1675036800, 1675353600],  # 2023-01-30 to 2023-02-02T16:00
             'energy_bins': [0, 4, 4.45, 4.95, 5.45, 5.95, 6.45, 6.95, 7.35, 7.75, 8.25, 8.75, 9.25, 10,
                             10.5, 11, 12, 13, 15, 18, 21, 25, 28, 32, 36, 43, 50, 59, 70, 84, 110, 150, np.inf]
         }
@@ -34,7 +36,7 @@ class StixEnergyBins:
             return StixEnergyBins._default_ebins
 
         for entry in StixEnergyBins._history:
-            if entry['time_ranges'][0] <= unix_time < entry['time_ranges'][1]:
+            if entry['time_range'][0] <= unix_time < entry['time_range'][1]:
                 return entry['energy_bins']
 
         return StixEnergyBins._default_ebins
